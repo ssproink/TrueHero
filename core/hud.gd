f5122059303b38abe3d64ebc12731aeac651851d
Sergey Skokov <sergskokow@gmail.com>
2024-11-05 07:50:28 +0300

    Enemy HP bar.

--- core/hud.gd
+++ core/hud.gd
@@ -9,6 +9,22 @@ extends Control
 			true_hero.SPChanged.connect(sp_changed)
 			$Bars/SPBar/Labels/ValueRegeneration.text = tr("+%dPER_SEC" % true_hero.sp_regen)
 
+@export var boss : Entity :
+	set(another_boss):
+		if another_boss != boss and boss != null:
+			boss.HPChanged.disconnect(boss_hp_changed)
+		boss = another_boss
+		if boss == null:
+			$Bars/EnemyBar.visible = false
+		else:
+			$Bars/EnemyBar.visible = true
+			boss.HPChanged.connect(boss_hp_changed)
+			$Bars/EnemyBar/Labels/ValueRegeneration.text = tr("+%dPER_SEC" % boss.hp_regen)
+
+func boss_hp_changed():
+	$Bars/EnemyBar.value = boss.hp
+	$Bars/EnemyBar/Labels/Value.text = str(boss.hp)
+
 func hp_changed():
 	$Bars/HPBar.value = true_hero.hp
 	$Bars/HPBar/Labels/Value.text = str(true_hero.hp)
